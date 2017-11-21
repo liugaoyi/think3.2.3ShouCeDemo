3b5ae95bfa473acd393f2e59ca3dc2950c3c5129
liup <1424964313@qq.com>
2017-11-21 15:00:38 +0800

    导入thinkphp核心包

---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/4
+ * Time: 13:00
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+class TestController extends Controller{
+    public function baoneiweixiu0(){
+        $this->display();
+    }
+
+}
